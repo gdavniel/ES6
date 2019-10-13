3045f73f915418f94bbda4758249d2df1a5cea22
Daniel <gdel.daniel@gmail.com>
2019-10-12 21:21:03 -0500

    template strings

--- app.js
+++ app.js
@@ -5,3 +5,31 @@ console.log(variable);
 const letra = `a`
 
 console.log(letra);
+
+//
+
+var nombre = 'luis'
+
+if (nombre) {
+    var nombre = 'carlos'
+}
+
+console.log(nombre); // carlos
+
+//
+
+let nombres = 'luis'
+
+if (nombres) {
+    let nombres = 'carlos'
+}
+
+console.log(nombres); // carlos
+
+//
+
+const trabajo = "medico"
+
+console.log("nombre: " + nombre + " trabajo: " + trabajo);
+
+console.log(`nombre: ${nombre} trabajo: ${trabajo}`);
